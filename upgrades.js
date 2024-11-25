5327b590f9f9ab91202061a3063981d24b369ae5
Hillo95 <148394208+Hillo95@users.noreply.github.com>
2024-11-25 10:55:26 -0600

    Update upgrades.js

--- upgrades.js
+++ upgrades.js
@@ -5,7 +5,6 @@ function buyUpgrade(section,toBuy) {
 }
 
 function setUpgradeInfo(section,i) {  // Hide When Mouse Leave Sometime
-  alert('yo');
   if (up[section][i].unlocked) {
     $("#infoNameT").text(up[section][i].displayName);
     $("#infoStockT").text(up[section][i].max_level ? 'Max Level' : `Level ${up[section][i].level} Out Of ${up[section][i].levels}`);
