35a5ff4872de9b282b6c2a98f7eb90f736b59e6e
Hillo95 <148394208+Hillo95@users.noreply.github.com>
2024-11-26 10:35:01 -0600

    Update upgrades.js

--- upgrades.js
+++ upgrades.js
@@ -1,10 +1,10 @@
-function buyUpgrade(section,toBuy) {
+function buyUpgrade(section,toBuy) { //maybe change if sometime if there are other currencies & change to compensate for untiered upgrades
   if ( (up[section][toBuy].unlocked) & (!up[section][toBuy].max_level) & (n.c >= up[section][toBuy].price[up[section][toBuy].level]) ) {
     up[section][toBuy].def();
   }
 }
 
-function setUpgradeInfo(section,i) {  // Hide When Mouse Leave Sometime
+function setUpgradeInfo(section,i) {  // Hide When Mouse Leave Sometime ***** done???
   if (up[section][i].unlocked) {
     $("#infoNameT").text(up[section][i].display_name);
     $("#infoStockT").text(up[section][i].max_level ? 'Max Level' : `Level ${up[section][i].level + 1} Out Of ${up[section][i].levels}`);
